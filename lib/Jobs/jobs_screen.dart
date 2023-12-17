a94b8523996a81c50f429b590d8f714e134759cd
Liul-Abera <liulaber43@gmail.com>
2023-12-17 18:38:34 -0500

    refactor: Use named parameter for clarity in BottomNavigationBarForApp

--- lib/Jobs/jobs_screen.dart
+++ lib/Jobs/jobs_screen.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:job_portal/user_state.dart';
+import 'package:job_portal/widgets/bottom_nav_bar.dart';
 
 class JobScreen extends StatefulWidget {
   const JobScreen({super.key});
@@ -13,19 +13,30 @@ class _JobScreenState extends State<JobScreen> {
   FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Jobs Screen'),
-      ),
-      body: ElevatedButton(
-        onPressed: () {
-          _auth.signOut();
-          Navigator.canPop(context) ? Navigator.pop(context) : null;
-
-          Navigator.pushReplacement(
-              context, MaterialPageRoute(builder: (_) => UserState()));
-        },
-        child: Text('Logout'),
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [Colors.deepOrange.shade300, Colors.blueAccent],
+        begin: Alignment.centerLeft,
+        end: Alignment.centerRight,
+        stops: const [0.2, 0.9],
+      )),
+      child: Scaffold(
+        bottomNavigationBar: BottomNavigationBarForApp(indexNum: 0),
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text('Job screen'),
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              colors: [Colors.deepOrange.shade300, Colors.blueAccent],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              stops: const [0.2, 0.9],
+            )),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,32 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:job_portal/Jobs/jobs_screen.dart';
+
+class BottomNavigationBarForApp extends StatelessWidget {
+  final int indexNum;
+  BottomNavigationBarForApp({required this.indexNum});
+  @override
+  Widget build(BuildContext context) {
+    return CurvedNavigationBar(
+      color: Colors.deepOrange.shade400,
+      backgroundColor: Colors.blueAccent,
+      buttonBackgroundColor: Colors.deepOrange.shade300,
+      index: indexNum,
+      items: [
+        Icon(
+          Icons.list,
+          size: 19,
+          color: Colors.black,
+        )
+      ],
+      onTap: (index) {
+        if (index == 0) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => JobScreen()),
+          );
+        }
+      },
+    );
+  }
+}
