0a99c90b05be9a836c0630c812c047cc0bd6addf
Liul-Abera <liulaber43@gmail.com>
2024-01-02 09:00:44 -0500

    The cancel button's onPressed callback checks if there is a previous route in the navigation stack using Navigator.canPop.  If there is, it pops the current route, effectively closing the current screen or dialog.  If there is no previous route, it does nothing, preventing unexpected behavior.

--- lib/Jobs/upload_job.dart
+++ lib/Jobs/upload_job.dart
@@ -123,6 +123,19 @@ class _UploadJobNowState extends State<UploadJobNow> {
               },
             ),
           ),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.canPop(context) ? Navigator.pop(context) : null;
+                },
+                child: Text(
+                  'Cancel',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ))
+          ],
         );
       },
     );
@@ -143,20 +156,20 @@ class _UploadJobNowState extends State<UploadJobNow> {
       child: Scaffold(
         bottomNavigationBar: BottomNavigationBarForApp(indexNum: 2),
         backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          title: const Text('Upload Job Now'),
-          centerTitle: true,
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.deepOrange.shade300, Colors.blueAccent],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-                stops: const [0.2, 0.9],
-              ),
-            ),
-          ),
-        ),
+        // appBar: AppBar(
+        //   title: const Text('Upload Job Now'),
+        //   centerTitle: true,
+        //   flexibleSpace: Container(
+        //     decoration: BoxDecoration(
+        //       gradient: LinearGradient(
+        //         colors: [Colors.deepOrange.shade300, Colors.blueAccent],
+        //         begin: Alignment.centerLeft,
+        //         end: Alignment.centerRight,
+        //         stops: const [0.2, 0.9],
+        //       ),
+        //     ),
+        //   ),
+        // ),
         body: Center(
           child: Padding(
             padding: EdgeInsets.all(7.0),
